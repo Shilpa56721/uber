3ae2081647dee52e9dae8b05cc347e7d4135697e
Shilpa56721 <imshilpakumari83@gmail.com>
2025-06-24 13:12:55 +0530

    login and signup page made

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import CaptainSignup from './pages/CaptainSignup'
+import Captainlogin from './pages/Captainlogin'
+import { UserDataContext } from './context/UserContext'
+
+
+
+
+const App = () => {
+  
+  return ( 
+    <div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<UserLogin />} />
+          <Route path='/signup' element={<UserSignup />} />
+          <Route path='/Captain-login' element={<Captainlogin />} />
+          <Route path='/captain-signup' element={<CaptainSignup />} />
+        </Routes>
+    </div>
+    )
+}
+
+export default App
